39bc3282611cb4368f2748f3aeecc3873a4840b3
Elijah Chileshe <chilesheelijah01@gmail.com>
2025-04-06 22:25:40 +0200

    Add MongoDB connection feature

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true,
     // useCreateIndex: true,
     useUnifiedTopology: true,
         }).then(()=> {
-        console.log("Successfully Connected to the Database In the Cloud")
+        console.log("Successfully Connected to the Database")
     }).catch((err) =>{
         console.log(err);
     });
