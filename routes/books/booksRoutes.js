4061a6f06d4ef607908880bd814891f2128958f7
Elijah Chileshe <chilesheelijah01@gmail.com>
2025-06-29 15:36:44 +0200

    updated get houses function to include pagination

--- routes/books/booksRoutes.js
+++ routes/books/booksRoutes.js
@@ -35,4 +35,34 @@ router.post("/", authMiddleware, async (req, res) => {
     }
 })
 
+
+
+router.get("/", authMiddleware, async (req, res) => {
+    try {
+
+        const page = req.query.page || 1;
+        const limit = req.query.limit || 5;
+        const skip = (page - 1) * limit;
+
+
+        const houses = await House.find()
+            .sort({createdAt: -1}) // desc order
+            .skip(skip)
+            .limit(limit)
+            .populate("user", "fullName profileImage")
+        // res.status(200).json({success: true, houses})
+
+        const total = await House.countDocuments();
+        const totalPages = Math.ceil(total / limit);
+        res.send({
+            houses,
+            currentPage: page,
+            totalHouses: total,
+            totalPages      
+        })
+    } catch (error) {
+        console.log("error getting records", error);
+        res.status(500).json({success: false, message: "Internal Error"})
+    }
+})
 module.exports = router;
